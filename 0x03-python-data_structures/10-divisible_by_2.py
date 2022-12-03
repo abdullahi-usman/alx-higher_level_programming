7c6f5ee664865be10d4e5c9b18d8deea1bf58e08
Abdullahi Usman <abdullahismn7@gmail.com>
2022-12-03 18:34:13 +0100

    add divisible by 2

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def divisible_by_2(my_list=[]):
+    new_list = []
+
+    for item in my_list:
+        new_list.append(False if item % 2 else True)
+
+    return (new_list)
+        
