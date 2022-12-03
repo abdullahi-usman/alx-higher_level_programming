17052f22e7e86a52a5c63ae91f97da95531b3a9c
Abdullahi Usman <abdullahismn7@gmail.com>
2022-12-03 18:24:28 +0100

    get max number

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def max_integer(my_list=[]):
+
+    if len(my_list) <= 0:
+        return None
+
+    res = 0
+    for item in my_list:
+        if item > res:
+            res = item
+    return (res)
