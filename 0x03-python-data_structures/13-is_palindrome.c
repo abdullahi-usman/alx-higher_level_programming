979d2e7e65a5c1c6c4eb39ec1e0b1a5fc470de68
Abdullahi Usman <abdullahismn7@gmail.com>
2022-12-03 19:44:10 +0100

    add palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 #include "lists.h"
 
+/**
+* is_palindrome - check if is palindrome
+* @head: the head
+* Return: True or false
+*/
 int is_palindrome(listint_t **head)
 {
-	return 1;
+	return (1);
 }
