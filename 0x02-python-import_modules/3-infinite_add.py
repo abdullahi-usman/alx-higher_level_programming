99fef2b65196ac448517141d4e4294ef61e85b1d
Abdullahi Usman <abdullahismn7@gmail.com>
2022-12-03 12:06:36 +0100

    add indefinite addition

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+from sys import argv
+
+if __name__ == "__main__":
+    result = 0
+    for arg in range(1, len(argv)):
+        result += int(argv[arg])
+    
+    print(result)
