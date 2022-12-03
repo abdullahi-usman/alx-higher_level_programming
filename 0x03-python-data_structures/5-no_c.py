75ecf28d7564b51dd949ba169514f31e3c76d324
Abdullahi Usman <abdullahismn7@gmail.com>
2022-12-03 16:44:58 +0100

    add no c

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
 
 def no_c(my_string):
-    return my_string.replace("C", "").replace("c", "")
+    new_str = list(my_string)
+
+    for index in range(0, len(new_str)):
+        if new_str[index].upper() == 'C':
+            new_str[index] = ''
+
+    return str().join(new_str)
