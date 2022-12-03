ac5001fecee9a7f82f124fca5659bb2c66eafd63
Abdullahi Usman <abdullahismn7@gmail.com>
2022-12-03 17:35:41 +0100

    add print matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
-    if (len(matrix) > 0 and len(matrix[0]) > 0):
+    if (len(matrix) > 0):
         for arr in matrix:
-            print("{:d} {:d} {:d}".format(arr[0], arr[1], arr[2]))
+            if (len(arr) == 3):
+                print("{:d} {:d} {:d}".format(arr[0], arr[1], arr[2]))
