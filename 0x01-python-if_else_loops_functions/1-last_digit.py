2bccf1cc1293fe420cd79656d3a6187b4edf7f68
Abdullahi Usman <abdullahismn7@gmail.com>
2022-12-05 00:12:18 +0100

    add guess last digit

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+
+number = str(number)
+last_num = number[len(number) - 1]
+word = "0"
+if int(last_num) > 5:
+    word = "greater than 5"
+elif int(last_num) < 6 and int(last_num) > 0:
+    word = "less than 6 and not 0"
+
+print(f"Last digit of {number} is {last_num} and is {word}")
