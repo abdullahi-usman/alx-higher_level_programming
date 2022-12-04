69c6eb7c3f230ad07deaa419244e06ca26c7f758
Abdullahi Usman <abdullahismn7@gmail.com>
2022-12-05 00:30:26 +0100

    add last digit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,9 +5,14 @@ number = random.randint(-10000, 10000)
 number = str(number)
 last_num = number[len(number) - 1]
 word = "0"
+
+if int(number) < 0:
+    last_num = str(-int(last_num))
+
 if int(last_num) > 5:
     word = "greater than 5"
-elif int(last_num) < 6 and int(last_num) > 0:
+elif int(last_num) < 6 and int(last_num) != 0:
     word = "less than 6 and not 0"
+
 
 print(f"Last digit of {number} is {last_num} and is {word}")
