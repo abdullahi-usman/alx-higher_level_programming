d273ed71110261a95a3c9aca035533fb8d6a4a24
Abdullahi Usman <abdullahismn7@gmail.com>
2022-12-05 04:52:27 +0100

    fix -- 5

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 
-print("{}".format(''.join([chr(alpha) for alpha in range(97, 123)]) \
-    .replace("q","").replace("e", "")))
+print("{}".format(''.join([chr(alpha) for alpha in range(97, 123)])
+    .replace("q", "").replace("e", "")), end="")
