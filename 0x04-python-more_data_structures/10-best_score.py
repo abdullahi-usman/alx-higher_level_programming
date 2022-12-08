22eaa4001c14b94526c1b9c30537949cf0f01d0d
Abdullahi Usman <abdullahismn7@gmail.com>
2022-12-08 03:08:20 +0100

    get best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,12 +2,14 @@
 
 
 def best_score(a_dictionary):
-    if a_dictionary == None:
+    if a_dictionary is None:
         return (None)
-    
-    res = 0
+
+    res = None
+    re_int = 0
     for mem in a_dictionary:
-        if a_dictionary[mem] > res:
-            res = a_dictionary[mem]
-    
-    return (res if res != None else None)
+        if a_dictionary[mem] is not None and a_dictionary[mem] > re_int:
+            res = mem
+            re_int = a_dictionary[mem]
+
+    return (res if res is not None else None)
