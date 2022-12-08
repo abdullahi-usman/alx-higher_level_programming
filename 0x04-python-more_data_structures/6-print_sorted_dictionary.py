b50317014a4764865357d33526018815a43fbae0
Abdullahi Usman <abdullahismn7@gmail.com>
2022-12-08 02:32:47 +0100

    print sorted

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def m_compare(str):
+    print(str[0])
+    return (ord(str[0]))
+
+
+def print_sorted_dictionary(a_dictionary):
+    dict_keys = []
+    for key in a_dictionary:
+        dict_keys.append(key)
+
+    dict_keys = sorted(dict_keys, key=m_compare)
+
+    for key in dict_keys:
+        print(f"{key} : {a_dictionary[key]}")
