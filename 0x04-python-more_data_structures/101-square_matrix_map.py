4347625e1260f297a0bfbbd063c845bbe8363afc
Abdullahi Usman <abdullahismn7@gmail.com>
2022-12-08 19:52:10 +0100

    add square matrix -- fix 2

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    return list(map(lambda arr: [arr[0] * arr[0],
-                arr[1] * arr[1], arr[2] * arr[2]], matrix))
+    return list(map(lambda arr: list(map(lambda x: x * x, arr)), matrix))
