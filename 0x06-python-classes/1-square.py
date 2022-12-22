997ee4cf48f10897c78ac8c33fb1135853459cee
Abdullahi Usman <abdullahismn7@gmail.com>
2022-12-22 03:21:19 +0100

    add private prop

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,11 +1,27 @@
 #!/usr/bin/python3
 
+""" Module Documetation
+    This module demonstrates documentation as specified by
+    the `Google Python Style Guide`_. Docstrings may extend
+    over multiple lines. Sections are created with a section
+    header and a colon followed by a block of indented text.
+"""
+
+
 class Square:
-    """ This module demonstrates documentation as specified by the `Google Python
-    Style Guide`_. Docstrings may extend over multiple lines. Sections are created
-    with a section header and a colon followed by a block of indented text.
-    
+    """ Class Documentation
+    This module demonstrates documentation as specified
+    by the `Google Python Style Guide`_. Docstrings may extend
+    over multiple lines. Sections are created with a section header
+    and a colon followed by a block of indented text.
     """
-    __size = 0;
-    def __init__(self, size = None) -> None:
+    __size = 0
+
+    def __init__(self, size=0) -> None:
+        """     Function Documentation
+        This module demonstrates documentation as specified
+        by the `Google Python Style Guide`_. Docstrings may extend
+        over multiple lines. Sections are created with a section
+        header and a colon followed by a block of indented text.
+        """
         __size = size
