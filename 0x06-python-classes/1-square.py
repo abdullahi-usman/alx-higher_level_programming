951ef83bf7861c55b5591b23224e138a076cde51
Abdullahi Usman <abdullahismn7@gmail.com>
2022-12-22 03:31:07 +0100

    add private member

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -15,13 +15,12 @@ class Square:
     over multiple lines. Sections are created with a section header
     and a colon followed by a block of indented text.
     """
-    __size = 0
 
-    def __init__(self, size=0) -> None:
+    def __init__(self, size):
         """     Function Documentation
         This module demonstrates documentation as specified
         by the `Google Python Style Guide`_. Docstrings may extend
         over multiple lines. Sections are created with a section
         header and a colon followed by a block of indented text.
         """
-        __size = size
+        self.__size = size
