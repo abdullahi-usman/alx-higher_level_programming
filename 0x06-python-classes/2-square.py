0ec332cab971994d18d3e2c1093dfa1a8773052a
Abdullahi Usman <abdullahismn7@gmail.com>
2022-12-22 03:37:59 +0100

    add init class

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,11 +1,32 @@
 #!/usr/bin/python3
 
+""" Module Documetation
+    This module demonstrates documentation as specified by
+    the `Google Python Style Guide`_. Docstrings may extend
+    over multiple lines. Sections are created with a section
+    header and a colon followed by a block of indented text.
+"""
+
+
 class Square:
-    """ This module demonstrates documentation as specified by the `Google Python
-    Style Guide`_. Docstrings may extend over multiple lines. Sections are created
-    with a section header and a colon followed by a block of indented text.
-    
+    """ Class Documentation
+    This module demonstrates documentation as specified
+    by the `Google Python Style Guide`_. Docstrings may extend
+    over multiple lines. Sections are created with a section header
+    and a colon followed by a block of indented text.
     """
-    __size = 0;
-    def __init__(self, size = None) -> None:
-        __size = size
+
+    def __init__(self, size=0):
+        """     Function Documentation
+        This module demonstrates documentation as specified
+        by the `Google Python Style Guide`_. Docstrings may extend
+        over multiple lines. Sections are created with a section
+        header and a colon followed by a block of indented text.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
