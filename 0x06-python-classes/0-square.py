d7b62a6c81f718b124f5fbf335c89876bc58f409
Abdullahi Usman <abdullahismn7@gmail.com>
2022-12-22 03:03:02 +0100

    add sqaure

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
 
+""" This module demonstrates documentation as specified by the `Google Python
+    Style Guide`_. Docstrings may extend over multiple lines. Sections are created
+    with a section header and a colon followed by a block of indented text.
+    
+"""
 class Square:
     """ This module demonstrates documentation as specified by the `Google Python
     Style Guide`_. Docstrings may extend over multiple lines. Sections are created
     with a section header and a colon followed by a block of indented text.
     
     """
-    pass
+    def __init__(self) -> None:
+        pass
