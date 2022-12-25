c94d04bd20e56466f2846ad75abb940019592c12
Abdullahi Usman <abdullahismn7@gmail.com>
2022-12-25 11:57:24 +0100

    fullfil requirements

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
 
-"""Add Integer Module """
+"""Add Integer Module 
+This module is part of this class
+And has the requierment to be atleast
+five lines long, so we are going
+to make it five lines long"""
+
 def add_integer(a, b=98):
-	"""Add two integers and return their result"""
+	"""Add two integers and return their result
+	Here also our line must be thre lines long
+	And at this line it is three line long"""
 	if (type(a) not in [int, float]):
 		raise TypeError('a must be an integer')
 	if (type(b) not in [int, float]):
