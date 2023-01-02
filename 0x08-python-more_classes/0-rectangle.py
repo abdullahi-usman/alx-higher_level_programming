224d5766d3082f4286482e63782815d1e6c5cb8f
Abdullahi Usman <abdullahismn7@gmail.com>
2023-01-02 14:02:55 +0100

    fix 2

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+"""Module documentation of this said class
+   And its infamous glory and all.
+   This is going to be a new start
+"""
+
 
 class Rectangle:
+    """Rectangle is a new class
+    meant to calculate the area of
+    rectangle and all that"""
     pass
