3ced7cdf2d2dab989d330794b6b6888c971ac8db
Abdullahi Usman <abdullahismn7@gmail.com>
2023-01-09 22:29:07 +0100

    is instance

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''
+Is an instance of this or the order
+'''
+
+
+def is_same_class(obj, a_class):
+    '''
+    This function test to see instance
+    '''
+    return type(obj) is a_class
