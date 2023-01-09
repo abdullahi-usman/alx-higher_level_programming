eddd749d63720452246a6247d455d9d54a90a6f1
Abdullahi Usman <abdullahismn7@gmail.com>
2023-01-09 23:35:09 +0100

    add rectangle

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+'''
+Rectangle Module of baseGeometry
+'''
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    '''
+    Rectangle MOdule derived fom base geometry
+    '''
+
+    def __init__(self, width, height):
+        '''
+        init function
+        '''
+        super().__init__()
+
+        super().integer_validator('width', width)
+        super().integer_validator('height', height)
+
+        self.__width = width
+        self.__height = height
+    
+    def area(self):
+        '''
+        Return the area of a rectangle
+        '''
+        return self.__height * self.__width
+    
+    def __str__(self):
+        
+        '''
+        Return the string representation
+        '''
+        return f"[Rectangle]{self.__width}/{self.__height}"
