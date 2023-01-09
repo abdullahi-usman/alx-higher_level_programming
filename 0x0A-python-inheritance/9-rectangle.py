45b65ff1c0c61b5990b2dc6b47247cda4d4c3d9b
Abdullahi Usman <abdullahismn7@gmail.com>
2023-01-09 23:35:59 +0100

    add pycodestyle

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -23,15 +23,15 @@ class Rectangle(BaseGeometry):
 
         self.__width = width
         self.__height = height
-    
+
     def area(self):
         '''
         Return the area of a rectangle
         '''
         return self.__height * self.__width
-    
+
     def __str__(self):
-        
+
         '''
         Return the string representation
         '''
