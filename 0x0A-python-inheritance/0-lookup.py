47cccbdc7cb17bfe932d7e3d5df02fc793879f3a
Abdullahi Usman <abdullahismn7@gmail.com>
2023-01-09 22:02:09 +0100

    look up

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''
+This module prints the dir attribute of the
+object and return
+
+'''
+
+
+def lookup(obj):
+    '''
+    Print dirs and return
+    make sure its pretty
+    '''
+    return dir(obj)
---
+++ 0x0A-python-inheritance/README.md
@@ -0,0 +1,2 @@
+Official README documentation of the finest
+object and tension
