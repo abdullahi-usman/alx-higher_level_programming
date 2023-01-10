a955370f7c7c153a0cafa3d63806278efd7c3bbd
Abdullahi Usman <abdullahismn7@gmail.com>
2023-01-10 20:19:21 +0100

    fix 3

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
 '''
-Module to print
+Module to print input
+and output of the file
 '''
+
+
+from asyncore import read
 
 
 def read_file(filename=""):
     '''
     Read the file and print it output
+    using utf-8 and make sure its close
     '''
     with open(filename, encoding="utf-8") as file:
-        print(file.read())
+        for line in file:
+            print(line)
