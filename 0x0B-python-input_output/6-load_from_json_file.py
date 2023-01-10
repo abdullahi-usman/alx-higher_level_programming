28bda88cbb78a81a531f1599ba033fab4ddbbe17
Abdullahi Usman <abdullahismn7@gmail.com>
2023-01-10 21:46:28 +0100

    loads and transform

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+'''
+FIle contains functions that loads
+string from file and loads the content
+into a json object
+'''
+
+import json
+
+
+def load_from_json_file(filename):
+    '''
+    load json from file and turns the
+    content into an object
+    '''
+    with open(filename) as file:
+        return json.loads(filename.read())
